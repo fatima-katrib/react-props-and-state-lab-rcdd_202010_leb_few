c370c70d9eda657b1845dfd3ddb1dc89e1db5b7c
jovial-application-1943 <fatima.katrib@lau.edu>
2021-02-01 07:57:13 +0000

    Automatically backed up by Learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -20,7 +20,7 @@ class Pet extends React.Component {
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
+            {this.props.pet.isAdopted ? (<button className="ui disabled button">Already adopted</button>
           <button className="ui primary button">Adopt pet</button>
         </div>
       </div>
