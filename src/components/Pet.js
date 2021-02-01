d2670c5828eb0aa1f86cab9e027ff4b33e25ce5a
jovial-application-1943 <fatima.katrib@lau.edu>
2021-02-01 07:54:53 +0000

    Automatically backed up by Learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -12,7 +12,7 @@ class Pet extends React.Component {
             PET NAME
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">PET TYPE: {this.props.pet.type}</span>
           </div>
           <div className="description">
             <p>Age: PET AGE</p>
