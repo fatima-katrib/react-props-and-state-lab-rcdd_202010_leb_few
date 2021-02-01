a225bbcc784598c9a736e95f91de0714dc174e2c
jovial-application-1943 <fatima.katrib@lau.edu>
2021-02-01 08:05:58 +0000

    Automatically backed up by Learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -6,7 +6,7 @@ class Pet extends React.Component {
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
+           
              {this.props.pet.name}{' '}
             {this.props.pet.gender === 'female' ? '♀' : '♂'}
             PET NAME
