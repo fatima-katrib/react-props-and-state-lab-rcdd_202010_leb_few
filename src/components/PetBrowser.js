0a71d0dc79eb9740986fe7b179e9c776efac48f8
jovial-application-1943 <fatima.katrib@lau.edu>
2021-02-01 08:07:58 +0000

    Automatically backed up by Learn

--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -4,6 +4,9 @@ import Pet from './Pet'
 
 class PetBrowser extends React.Component {
   render() {
+    const petCards = this.props.pets.map(pet => (
+      <Pet pet={pet} key={pet.id} onAdoptPet={this.props.onAdoptPet} />
+    ));
     return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
   }
 }
